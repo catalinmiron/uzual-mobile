48381624f72ae2f2c92a903d411927abdf6f925d
catalinmiron <mironcatalin@gmail.com>
2019-04-30 16:07:33 +0200

    Increase font size to tabbar icon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -8,7 +8,7 @@ export default class TabBarIcon extends React.Component {
     return (
       <Icon.Ionicons
         name={this.props.name}
-        size={26}
+        size={28}
         style={{ marginBottom: -3 }}
         color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
       />
