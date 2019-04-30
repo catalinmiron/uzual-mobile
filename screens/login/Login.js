d5d42fcabbba79c66c21009e4ce157597993c6ab
catalinmiron <mironcatalin@gmail.com>
2019-04-30 16:08:30 +0200

    fix indendation

--- screens/login/Login.js
+++ screens/login/Login.js
@@ -42,15 +42,15 @@ export default class Login extends React.Component {
 
   render() {
     return <View style={{ flex: 1, alignItems: "center", justifyContent: "center", width: 260, alignSelf: "center" }}>
-    <MonoText style={{fontFamily: "Menlo", fontSize: 42}}>UZUAL</MonoText>
-    <MonoText style={{fontSize: 12, color: 'rgba(0,0,0,0.2)', marginBottom: 60, textAlign:"center"}}>Feed your brain with habits for a better mood</MonoText>
-    <MonoText style={{alignSelf: "flex-start", marginBottom: 10, color: 'rgba(0,0,0,0.4)',}}>Email</MonoText>
-    <TextInput defaultValue={this.state.email} autoCapitalize={"none"} onChangeText={(e) => this._change('email', e.toLowerCase())} style={{height: 40, borderBottomColor: "#d3d3d3", borderBottomWidth:2 , width: 260, fontFamily: 'space-mono', marginBottom: 40}}/>
-    <MonoText style={{alignSelf: "flex-start", marginBottom: 10, color: 'rgba(0,0,0,0.4)',}}>Password</MonoText>
-    <TextInput defaultValue={this.state.password} secureTextEntry onChangeText={(e) => this._change('password', e)} style={{height: 40, borderBottomColor: "#d3d3d3", borderBottomWidth:2 , width: 260, fontFamily: 'space-mono', marginBottom: 40}}/>
-    <TouchableOpacity onPress={this._login} style={{alignItems: "center", justifyContent: "center", backgroundColor: 'turquoise', width: 260, height: 50, marginTop: 50}}>
-      <MonoText style={{color: "#fff"}}>LOGIN</MonoText>
-    </TouchableOpacity>
-  </View>
+      <MonoText style={{fontFamily: "Menlo", fontSize: 42}}>UZUAL</MonoText>
+      <MonoText style={{fontSize: 12, color: 'rgba(0,0,0,0.2)', marginBottom: 60, textAlign:"center"}}>Feed your brain with habits for a better mood</MonoText>
+      <MonoText style={{alignSelf: "flex-start", marginBottom: 10, color: 'rgba(0,0,0,0.4)',}}>Email</MonoText>
+      <TextInput defaultValue={this.state.email} autoCapitalize={"none"} onChangeText={(e) => this._change('email', e.toLowerCase())} style={{height: 40, borderBottomColor: "#d3d3d3", borderBottomWidth:2 , width: 260, fontFamily: 'space-mono', marginBottom: 40}}/>
+      <MonoText style={{alignSelf: "flex-start", marginBottom: 10, color: 'rgba(0,0,0,0.4)',}}>Password</MonoText>
+      <TextInput defaultValue={this.state.password} secureTextEntry onChangeText={(e) => this._change('password', e)} style={{height: 40, borderBottomColor: "#d3d3d3", borderBottomWidth:2 , width: 260, fontFamily: 'space-mono', marginBottom: 40}}/>
+      <TouchableOpacity onPress={this._login} style={{alignItems: "center", justifyContent: "center", backgroundColor: 'turquoise', width: 260, height: 50, marginTop: 50}}>
+        <MonoText style={{color: "#fff"}}>LOGIN</MonoText>
+      </TouchableOpacity>
+    </View>
   }
 }
