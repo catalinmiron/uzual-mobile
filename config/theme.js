0590158ed2a82533d2b6b41e993c1fdb81329d4c
catalinmiron <mironcatalin@gmail.com>
2019-05-06 07:29:44 +0200

    Add theme file #1

---
+++ config/theme.js
@@ -0,0 +1,79 @@
+const colors = {
+  white: '#FFFFFF',
+  bg: '#FAFAFA',
+  primary: 'turquoise',
+  text: '#0A2032',
+  default: '#333',
+  placeholder: '#9B9B9B',
+  shadow: '#ddd',
+  subheading: '#6D6D6D'
+};
+
+const spacing = {
+  small: 10,
+  normal: 14,
+  big: 20,
+  default: 10,
+  huge: 30,
+  unknown: 12
+};
+
+const size = {
+  stiny: 10,
+  tiny: 12,
+  default: 14,
+  medium: 16,
+  large: 24,
+  xlarge: 32,
+  xxlarge: 48,
+  xxxlarge: 52
+};
+
+const height = {
+  input: 70,
+  button: 60
+};
+
+const fontWeight = {
+  light: '300',
+  regular: '400',
+  normal: '500',
+  bold: '700',
+  extrabold: '900',
+  default: '500',
+  consi: '400',
+  //
+  big: '700',
+  large: '700',
+  xlarge: '700'
+};
+const fontWeights = {
+  small: '500',
+  default: '500',
+  medium: '500',
+  big: '700',
+  large: '700',
+  xlarge: '700'
+};
+
+const avatar = {
+  small: 50,
+  default: 50,
+  big: 60,
+  huge: 140,
+  light: 40
+};
+const layout = {
+  radius: 10,
+  smallRadius: 7
+};
+
+export default {
+  colors,
+  spacing,
+  layout,
+  size,
+  fontWeight,
+  avatar,
+  height
+};
