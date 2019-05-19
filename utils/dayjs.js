d03a1b041ba7ed20df2329db3fa8a0877b9da371
Catalin Miron <mironcatalin@gmail.com>
2019-05-19 23:01:26 +0200

    Write a comment to remind why we need to use memogit add -p ! #6

--- utils/dayjs.js
+++ utils/dayjs.js
@@ -9,6 +9,12 @@ const month = dayInstance.month();
 const daysInMonth = dayInstance.daysInMonth();
 const year = dayInstance.year();
 
+// Memoize
+// Expose a method that will accept an argument (Month)
+// and it will return the list of days as Array[]
+// This is going to be useful when we'll have a month
+// picker on Habit screen but also for the Mood screen
+// because there you can also filter the current month.
 const days = [...Array(daysInMonth - 1).keys()].map(u => {
   const date = start.add(u, 'day').format('YYYY-MM-DD');
 
