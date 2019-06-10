6ff775d99d84f81c28577dfb5bf47e2810f06e13
Catalin Miron <mironcatalin@gmail.com>
2019-06-10 23:20:49 +0200

    setmood mutation + offline + optimisticResponse #12

--- screens/mood/Mood.js
+++ screens/mood/Mood.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
+import queries from './queries.gql';
 import FullLoading from '../../components/FullLoading';
 import { Body, Heading, Wrapper, Scroll } from '../../components/styled';
 import MoodGraph from '../../components/MoodGraph';
-import { start, end, current, TIME_FORMAT } from '../../utils/dayjs';
+import DayMood from '../../components/DayMood';
+import { start, current, end, TIME_FORMAT } from '../../utils/dayjs';
+import { POLL_INTERVAL } from '../../constants/vars';
+
 export default class Mood extends React.Component {
   static navigationOptions = {
     header: null
+  };
+
+  _onSetMood = async type => {
+    const date = current.format(TIME_FORMAT);
+    await this.props.data.stopPolling();
+    this.props.setMood({
+      variables: {
+        date,
+        type
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        setMood: {
+          __typename: 'Mood',
+          date,
+          type,
+          id: uuid()
+        }
+      },
+      update: (proxy, { data: { setMood } }) => {
+        try {
+          const data = proxy.readQuery({
+            query: queries.moods,
+            variables: { start, end }
+          });
+
+          const lastMood = data.moods.slice(-1);
+          if (lastMood[0].date.startsWith(date)) {
+            data.moods.splice(-1, 1, setMood);
+          } else {
+            data.moods.push(setMood);
+          }
+
+          proxy.writeQuery({
+            query: queries.moods,
+            variables: { start, end },
+            data
+          });
+          this.props.data.startPolling(POLL_INTERVAL);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    });
   };
 
   _renderX = () => {
